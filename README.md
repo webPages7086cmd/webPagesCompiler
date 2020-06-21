3de182ef7404906f47bcb699e5d9861ea00eeee4
7086cmd <54303040+7086cmd@users.noreply.github.com>
2020-06-21 10:33:00 +0800

    提交信息

---
+++ README.md
@@ -0,0 +1,10 @@
+# webPages Compiler
+**webPages**是代码制作网页的方便、快捷的方法，且不改变HTML的多功能。
+
+`webPages Compiler`是它的编译器。
+
+使用Electron制作的窗口，更加美观，又不失去软件的多功能。
+
+我们仿照macOS的控制按钮与菜单，更加美观，且底部变成了白色，更加显得简洁。
+
+如果您需要详细讲解，请查看：[https://webPages7086cmd.github.io/webPagesCompilerDocument](https://webPages7086cmd.github.io/webPagesCompilerDocument)
--- main.js
+++ main.js
@@ -1,21 +1,39 @@
-const {app,BrowserWindow,Menu,ipcMain,ipcRenderer}=require('electron');
+const {app,BrowserWindow,Menu,ipcMain,ipcRenderer,screen}=require('electron');
 const express=require('express')
 const path=require('path');
 const fs=require('fs');
 app.on('ready',()=>{
+	let screenWidth=require('electron').screen.getPrimaryDisplay().workAreaSize.width;
+	let screenHeight=require('electron').screen.getPrimaryDisplay().workAreaSize.height;
 	let server=express();
 	server.get('*',(req,res)=>{
 		let page=fs.readFileSync(path.resolve(__dirname, './start.html'), 'utf-8')
 	});
 	server.listen(8080);
 	Menu.setApplicationMenu(null);
+	let menuWindow=new BrowserWindow({
+		x:0,
+		y:0,
+		width:screenWidth,
+		height:parseInt((screenHeight-768)/2),
+		movable:false,
+		resizeable:false,
+		frame:false,
+		transparent:true,
+		webPreferences:{
+			nodeIntegration:true
+		}
+	});
+	menuWindow.loadURL(path.join('file://',__dirname,'./startMenu.html'));
+	// menuWindow.maximize();
+	// menuWindow.unmaximize();
 	let openingWindow=new BrowserWindow({
 		width:1024,
 		height:768,
 		movable:true,
 		resizeable:true,
 		frame:false,
-		// transparent:true,
+		transparent:true,
 		webPreferences:{
 			nodeIntegration:true
 		}
@@ -24,7 +42,8 @@ app.on('ready',()=>{
 	openingWindow.loadURL(path.join('file://',__dirname,'./start.html'));
 	
 	ipcMain.on('closeWindow',function(){
-		app.quit();
+		openingWindow.close();
+		menuWindow.close();
 	});
 	ipcMain.on('minWindow',function(){
 		openingWindow.minimize();
@@ -37,6 +56,27 @@ app.on('ready',()=>{
 			openingWindow.maximize();
 		}
 	});
+	let aboutWindow;
+	ipcMain.on('about',function(){
+		aboutWindow=new BrowserWindow({
+			width:600,
+			height:400,
+			movable:true,
+			resizeable:true,
+			frame:false,
+			// transparent:true,
+			webPreferences:{
+				nodeIntegration:true
+			}
+		});
+		aboutWindow.loadURL(path.join('file://',__dirname,'./about.html'));
+		aboutWindow.on('close',()=>{
+			aboutWindow=null;
+		});
+	});
+	ipcMain.on('closeAboutWindow',function(){
+		aboutWindow.close();
+	});
 	ipcMain.on('editMode',function(){
 		let editWindow=new BrowserWindow({
 			width:1024,
@@ -46,6 +86,7 @@ app.on('ready',()=>{
 			transparent:true
 		});
 		openingWindow.close();
+		editWindow.loadURL(path.join('file://',__dirname,'./editingMode.html'));
 		editWindow.on('close',()=>{
 			editWindow=null;
 		});
@@ -58,10 +99,14 @@ app.on('ready',()=>{
 			resizeable:true,
 			transparent:true
 		});
+		editWindow.loadURL(path.join('file://',__dirname,'./changingMode.html'));
 		openingWindow.close();
 		changeWindow.on('close',()=>{
 			changeWindow=null;
 		});
+	});
+	openingWindow.on('close',function(){
+		openingWindow=null
 	});
 
 });
